de5aaf75c24641dfa5281397723583fd82d9f4a2
Subin <ewaterbin@naver.com>
2022-05-25 18:24:34 +0900

    프로미스 함수 외우기

---
+++ promise.js
@@ -0,0 +1,19 @@
+const getHen = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => resolve("🐓"), 1000);
+  });
+
+const getEgg = (hen) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => resolve(`${hen} => 🥚`), 1000);
+  });
+
+const cook = (egg) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => resolve(`${egg} => 🍳`), 1000);
+  });
+
+getHen()
+  .then((hen) => getEgg(hen))
+  .then((egg) => cook(egg))
+  .then((meal) => console.log(meal));
